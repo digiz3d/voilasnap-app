40509283461699139286488e7d6b6f2332ffc9e0
Léandre Daumont <digiz3d@users.noreply.github.com>
2019-11-16 19:41:48 +0100

    + Setup eslint, prettier

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { createAppContainer } from "react-navigation";
-import MainSwitchNavigator from "./navigation/MainSwitchNavigator";
+import React from 'react'
+import { createAppContainer } from 'react-navigation'
+import MainSwitchNavigator from './src/navigation/MainSwitchNavigator'
 
-const AppNavigationContainer = createAppContainer(MainSwitchNavigator);
+const AppNavigationContainer = createAppContainer(MainSwitchNavigator)
 
 export default class App extends React.Component {
   render() {
-    return <AppNavigationContainer />;
+    return <AppNavigationContainer />
   }
 }
