f9ae9d0f2873d26a8bc17b2240227a0dc87c7ce4
phoz0n <33037084+phoz0n@users.noreply.github.com>
2020-05-11 18:00:50 +0200

    Update/Adding icons to the bottom menu

--- src/navigation/ConnectedTabNavigator.js
+++ src/navigation/ConnectedTabNavigator.js
@@ -1,19 +1,48 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
-import Messages from '../screens/Messages'
-import Pictures from '../screens/Pictures'
+import Chat from '../screens/Chat'
+import Profile from '../screens/Profile'
 import Camera from '../screens/Camera'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
 const Tab = createBottomTabNavigator()
 
 const ConnectedTabNavigator = () => (
   <Tab.Navigator
-    screenOptions={{
-      headerShown: false,
+    initialRouteName="Chat"
+    tabBarOptions={{
+      activeTintColor: '#e91e63',
     }}>
-    <Tab.Screen name="Chat" component={Messages} />
-    <Tab.Screen name="Camera" component={Camera} />
-    <Tab.Screen name="Pic" component={Pictures} />
+    <Tab.Screen
+      name="Chat"
+      component={Chat}
+      options={{
+        tabBarLabel: 'Chat',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="chat" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Camera"
+      component={Camera}
+      options={{
+        tabBarLabel: 'Camera',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="camera" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={Profile}
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 )
 
