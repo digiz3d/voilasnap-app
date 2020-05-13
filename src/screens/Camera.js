6797df75af8d08376b0e922ec7ad095d784245bd
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2020-05-13 19:29:40 +0200

    Camera takes all available space

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { Text, View, TouchableOpacity, Dimensions } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
 import { Camera } from 'expo-camera'
 
-export default function App() {
+const FORMAT_HEIGHT = 16
+const FORMAT_WIDTH = 9
+
+export default function Cam() {
   const [hasPermission, setHasPermission] = useState(null)
   const [type, setType] = useState(Camera.Constants.Type.back)
+  const [availableSpace, setAvailableSpace] = useState({ height: 0, width: 0 })
+  const cameraRef = useRef()
 
   useEffect(() => {
     ;(async () => {
@@ -20,39 +25,56 @@ export default function App() {
     return <Text>No access to camera</Text>
   }
 
-  const width = Dimensions.get('screen').width
-  const height = (width * 16) / 9
+  let width = availableSpace.width
+  let height = (width * FORMAT_HEIGHT) / FORMAT_WIDTH
+
+  if (height < availableSpace.height) {
+    height = availableSpace.height
+    width = (height * FORMAT_WIDTH) / FORMAT_HEIGHT
+  }
+
+  const canRenderCamera = !!(availableSpace.width && availableSpace.height)
 
   return (
-    <View style={{ flex: 1 }}>
-      <Camera
-        style={{ width, height: Math.floor(height) }}
-        type={type}
-        ratio="16:9"
-        useCamera2Api={true}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          <TouchableOpacity
+    <View
+      style={{ flex: 1 }}
+      onLayout={(e) =>
+        setAvailableSpace({
+          height: e.nativeEvent.layout.height,
+          width: e.nativeEvent.layout.width,
+        })
+      }>
+      {canRenderCamera && (
+        <Camera
+          style={{ width, height: Math.floor(height) }}
+          type={type}
+          ratio={`${FORMAT_HEIGHT}:${FORMAT_WIDTH}`}
+          useCamera2Api={true}
+          ref={cameraRef}>
+          <View
             style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back,
-              )
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
             }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+            <TouchableOpacity
+              style={{
+                flex: 0.1,
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+              }}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back,
+                )
+              }}>
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>Flip</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
     </View>
   )
 }
